ff88756fe864a45930be3c93d3ff57b09574df1f
omar2209 cr7 <omardambrogi34@gmail.com>
2025-05-16 09:42:08 +0000

    Punto 4

--- Prenotazioni/src/app/app.component.ts
+++ Prenotazioni/src/app/app.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Prenotazioni } from './models/prenotazioni.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Prenotazioni';
   vettPrenotazioni: Prenotazioni[] = []
+  loading!: boolean
+  o_vettPrenotazioni!: Observable<Prenotazioni[]>
+  http!: HttpClient
+
+  constructor(http: HttpClient){this.http = http}
+
+  makeGet()
+  {
+    this.loading = true
+    this.o_vettPrenotazioni = this.http.get<Prenotazioni[]>("https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni")
+    this.o_vettPrenotazioni.subscribe(this.getData)
+  }
+
+  getData = (dati: Prenotazioni[]) =>
+  {
+    this.vettPrenotazioni = dati
+    this.loading = false
+    console.log(this.vettPrenotazioni)
+  }
+
+
+
+  ngOnInit()
+  {
+    this.makeGet()
+  }
 }
